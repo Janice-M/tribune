8f2f130e0a59bb879572d04ea4b0ee87b2fe81ea
Janice-M <janiceink001@gmail.com>
2019-08-21 10:01:29 +0300

    added bootstrap4 and set it up at tribune/settings.py

--- news/views.py
+++ news/views.py
@@ -1,6 +1,6 @@
 from django.http  import HttpResponse,Http404
 import datetime as dt
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 
 # Create your views here.
 def welcome(request):
@@ -15,6 +15,10 @@ def past_days_news(request,past_date):
     except ValueError:
         # Raise 404 error when ValueError is thrown
         raise Http404()
+        assert False
+    if date == dt.date.today():
+        return redirect(news_of_day)
+    return render(request, 'all-news/past-news.html', {"date": date})
 #..........
 def convert_dates(dates):
     
@@ -29,7 +33,7 @@ def convert_dates(dates):
 
 def news_of_day(request):
     date = dt.date.today()
-
+    return render(request, 'all-news/today-news.html', {"date": date,})
     # FUNCTION TO CONVERT DATE OBJECT TO FIND EXACT DAY
     day = convert_dates(date)
     html = f'''
