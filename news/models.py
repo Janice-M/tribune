dc905d1af2ea06ea70ecc920e3bccca097afe1a5
Janice-M <janiceink001@gmail.com>
2019-08-27 16:45:33 +0300

    article model editted

--- news/models.py
+++ news/models.py
@@ -15,6 +15,7 @@ class Editor(models.Model):
     
     def save_editor(self):
         self.save()
+        
     class Meta:
         ordering = ['first_name']
     
@@ -27,17 +28,28 @@ class tags(models.Model):
 class Article(models.Model):
     title = models.CharField(max_length =60)
     post = models.TextField()
-    editor = models.ForeignKey(Editor)
+    editor = models.ForeignKey(Editor, on_delete=model.DO_NOTHING)
     tags = models.ManyToManyField(tags)
     pub_date = models.DateTimeField(auto_now_add=True)
+    article_image = models.ImageField(upload_to = 'articles/', default='')
     
-
+    def__str__(self):
+        return self.title
+    
+    def save_article(self):
+        self.save()
+        
+    def delete_Article(self):
+        self.delete()
+        
     @classmethod
     def todays_news(cls):
         today = dt.date.today()
         news = cls.objects.filter(pub_date__date = today)
         return news
-    
+    @classmethod
+    def days_news(cls, date):
+        news=cls.objects.filter(pub_date__date=date)
     @classmethod
     def search_by_title(cls,search_term):
         news = cls.objects.filter(title__icontains=search_term)
