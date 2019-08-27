27debaa8e1303760932c42281e35608ed92bc461
Janice-M <janiceink001@gmail.com>
2019-08-27 16:48:23 +0300

    added a url pattern

--- news/urls.py
+++ news/urls.py
@@ -7,6 +7,7 @@ urlpatterns=[
     url(r'^$',views.news_of_day ,name='today-news'),
     url(r'^archives/(\d{4}-\d{2}-\d{2})/$',views.past_days_news,name = 'past_date'),
     url(r'^search/', views.search_results, name='search_results'),
+    url(r'^article/(\d+)', views.article, name= 'article')
 ]
 
 if settings.DEBUG:
