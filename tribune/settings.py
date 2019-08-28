e8646371acb761d2e475fef89d7eeb9f6211d52b
Janice-M <janiceink001@gmail.com>
2019-08-28 12:30:48 +0300

    edited settings for easier configuration

--- tribune/settings.py
+++ tribune/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/1.11/ref/settings/
 """
 
 import os
+from decouple import config 
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -104,6 +105,13 @@ AUTH_PASSWORD_VALIDATORS = [
     },
 ]
 
+
+# Email configurations remember to install python-decouple
+EMAIL_USE_TLS = True
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_PORT =  587
+EMAIL_HOST_USER = 'kokonutkoala@gmail.com')
+EMAIL_HOST_PASSWORD = 'Bookdrive1234'
 
 # Internationalization
 # https://docs.djangoproject.com/en/1.11/topics/i18n/
