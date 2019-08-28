6cbb59540480110c04942a649485e03d318529cc
Janice-M <janiceink001@gmail.com>
2019-08-28 17:45:29 +0300

    edits on past days news function

--- news/views.py
+++ news/views.py
@@ -58,22 +58,6 @@ def past_days_news(request,past_date):
     return render(request, 'all-news/past-news.html', {"date": date,'news':news})
 #..........
 
-def news_of_day(request):
-    date = dt.date.today()
-    news = Article.todays_news()
-
-    
-    return render(request, 'all-news/today-news.html', {"date": date, "news":news})
-    # FUNCTION TO CONVERT DATE OBJECT TO FIND EXACT DAY
-    day = convert_dates(date)
-    html = f'''
-        <html>
-            <body>
-                <h1>News for {day} {date.day}-{date.month}-{date.year}</h1>
-            </body>
-        </html>
-            '''
-    return HttpResponse(html)
 
 def past_days_news(request,past_date):
     try:
