394b66d52b1620a9df362246285b94e8413df979
Janice-M <janiceink001@gmail.com>
2019-08-28 17:42:02 +0300

    edits on news of day function

--- news/views.py
+++ news/views.py
@@ -29,6 +29,17 @@ def news_of_day(request):
         form=NewsLetterForm()    
     return render(request,'all-news/today-news.html',{'date':date,'news':news,'letterForm':form})    
 
+def convert_dates(dates):
+    
+    # Function that gets the weekday number for the date.
+    day_number = dt.date.weekday(dates)
+
+    days = ['Monday','Tuesday','Wednesday','Thursday','Friday','Saturday',"Sunday"]
+
+    # Returning the actual day of the week
+    day = days[day_number]
+    return day
+
 
 def past_days_news(request,past_date):
     
@@ -44,16 +55,6 @@ def past_days_news(request,past_date):
         return redirect(news_of_day)
     return render(request, 'all-news/past-news.html', {"date": date})
 #..........
-def convert_dates(dates):
-    
-    # Function that gets the weekday number for the date.
-    day_number = dt.date.weekday(dates)
-
-    days = ['Monday','Tuesday','Wednesday','Thursday','Friday','Saturday',"Sunday"]
-
-    # Returning the actual day of the week
-    day = days[day_number]
-    return day
 
 def news_of_day(request):
     date = dt.date.today()
