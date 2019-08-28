4d59682ba2006decf4ad32fceca84bc6198784d5
Janice-M <janiceink001@gmail.com>
2019-08-28 17:44:50 +0300

    edits on past days news function

--- news/views.py
+++ news/views.py
@@ -53,7 +53,9 @@ def past_days_news(request,past_date):
         assert False
     if date == dt.date.today():
         return redirect(news_of_day)
-    return render(request, 'all-news/past-news.html', {"date": date})
+    news=Article.days_news(date)
+    
+    return render(request, 'all-news/past-news.html', {"date": date,'news':news})
 #..........
 
 def news_of_day(request):
