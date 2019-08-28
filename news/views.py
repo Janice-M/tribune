cf0e313ce5c7beb66fa8ad985895e37cc0d27524
Janice-M <janiceink001@gmail.com>
2019-08-28 17:41:25 +0300

    edits on news of day function

--- news/views.py
+++ news/views.py
@@ -3,11 +3,32 @@ from django.shortcuts import render,redirect
 from django.http import HttpResponse,Http404,HttpResponseRedirect
 from .models import Article,NewsLetterRecipients
 from .forms import NewsLetterForm
-from .email import send_welcome_email
+from .email import send_welcome_emaile
 
 # Create your views here.
 def welcome(request):
     return render(request, 'welcome.html')
+
+def news_of_day(request):
+    date=dt.date.today()
+    news=Article.todays_news()
+    
+    if request.method=='POST':
+        form=NewsLetterForm(request.POST)
+        if form.is_valid():
+            name=form.cleaned_data['your_name']
+            email=form.cleaned_data['email']
+            recipient=NewsLetterRecipients(name=name,email=email)
+            recipient.save()
+
+            send_welcome_email(name,email)
+
+            HttpResponseRedirect('news_of_day')
+
+    else:
+        form=NewsLetterForm()    
+    return render(request,'all-news/today-news.html',{'date':date,'news':news,'letterForm':form})    
+
 
 def past_days_news(request,past_date):
     
