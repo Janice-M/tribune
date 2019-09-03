f4a545f2d2879df6e3b78acec633a74ef3690bd4
Janice-M <janiceink001@gmail.com>
2019-09-03 15:20:09 +0300

    removed editor tests and worked on the other tests

--- news/tests.py
+++ news/tests.py
@@ -1,6 +1,9 @@
 from django.test import TestCase
 from .models import Editor,Article,Tags
 import datetime as dt
+from django.test import TestCase
+from .models import Editor,Article,Tags
+import datetime as dt
 
 # Create your tests here.
 
@@ -8,7 +11,7 @@ class EditorTestClass(TestCase):
 
     #set up method
     def setUp(self):
-        self.brian=Editor(first_name='Brian',last_name='Sunday',email='brians931@gmail.com')
+        self.grey=Editor(first_name='grey',last_name='worm',email='grey@gmail.com')
 
     def test_instance(self):
         '''
@@ -26,8 +29,8 @@ class ArticleTestClass(TestCase):
 
     def setUp(self):
         # Creating a new editor and saving it
-        self.james= Editor(first_name = 'James', last_name ='Muriuki', email ='james@moringaschool.com')
-        self.james.save_editor()
+        self.janice= Editor(first_name = 'Janice', last_name ='Muthoni', email ='jmuthoni@moringaschool.com')
+        self.janice.save_editor()
 
         # Creating a new tag and saving it
         self.new_tag = Tags(name = 'testing')
@@ -51,4 +54,54 @@ class ArticleTestClass(TestCase):
         test_date = '2017-03-17'
         date = dt.datetime.strptime(test_date, '%Y-%m-%d').date()
         news_by_date = Article.days_news(date)
-        self.assertTrue(len(news_by_date) == 0) 
+        self.assertTrue(len(news_by_date) == 0)
+# Create your tests here.
+
+class EditorTestClass(TestCase):
+
+    #set up method
+    def setUp(self):
+        self.golda=Editor(first_name='golda',last_name='nkirote',email='nkirote@gmail.com')
+
+    def test_instance(self):
+        '''
+        Testing Instance
+        '''
+        self.assertTrue(isinstance(self.brian,Editor))
+
+    # Testing Save Method
+    def test_save_method(self):
+        self.golda.save_editor()
+        editors = Editor.objects.all()
+        self.assertTrue(len(editors) > 0)  
+
+class ArticleTestClass(TestCase):
+
+    ''' def setUp(self):
+        # Creating a new editor and saving it
+        self.salif= Editor(first_name = 'Salif', last_name ='Keita', email ='salifkey@moringaschool.com')
+        self.salif.save_editor()
+
+        # Creating a new tag and saving it
+        self.new_tag = Tags(name = 'testing')
+        self.new_tag.save()
+
+        self.new_article= Article(title = 'Test Article',post = 'This is a random test Post',editor = self.salif)
+        self.new_article.save()
+
+        self.new_article.tags.add(self.new_tag)
+
+    def tearDown(self):
+        Editor.objects.all().delete()
+        Tags.objects.all().delete()
+        Article.objects.all().delete()
+
+    def test_get_news_today(self):
+        today_news = Article.todays_news()
+        self.assertTrue(len(today_news)>0)
+
+    def test_get_news_by_date(self):
+        test_date = '2017-03-17'
+        date = dt.datetime.strptime(test_date, '%Y-%m-%d').date()
+        news_by_date = Article.days_news(date)
+        self.assertTrue(len(news_by_date) == 0)  '''
