d2eb462af63d5dd634e51e89d034216432e3344b
Janice-M <janiceink001@gmail.com>
2019-09-03 15:24:27 +0300

    views.py editted

--- news/views.py
+++ news/views.py
@@ -2,12 +2,10 @@ import datetime as dt
 from django.shortcuts import render,redirect
 from django.http import HttpResponse,Http404,HttpResponseRedirect
 from .models import Article,NewsLetterRecipients
-from .forms import NewsLetterForm
+from .forms import NewsLetterForm,NewArticleForm
 from .email import send_welcome_email
+from django.contrib.auth.decorators import login_required
 
-# Create your views here.
-def welcome(request):
-    return render(request, 'welcome.html')
 
 def news_of_day(request):
     date=dt.date.today()
@@ -30,67 +28,74 @@ def news_of_day(request):
     return render(request,'all-news/today-news.html',{'date':date,'news':news,'letterForm':form})    
 
 def convert_dates(dates):
-    
-    # Function that gets the weekday number for the date.
-    day_number = dt.date.weekday(dates)
+    '''
+    Function that returns day of the week
+    '''
+    #get weekday number 
+    day_number=dt.date.weekday(dates)
 
-    days = ['Monday','Tuesday','Wednesday','Thursday','Friday','Saturday',"Sunday"]
+    days=['Monday','Tuesday','Wednesday','Thursday','Friday','Saturday','Sunday']
 
-    # Returning the actual day of the week
-    day = days[day_number]
-    return day
-
+    #Returning the actual day of the week
+    day=days[day_number]
+    return day  
 
 def past_days_news(request,past_date):
-    
-    try:
-        # Converts data from the string Url
-        date = dt.datetime.strptime(past_date,'%Y-%m-%d').date()
 
+    try:
+        #converts data from the string url
+        date=dt.datetime.strptime(past_date,'%Y-%m-%d').date()
+        
     except ValueError:
-        # Raise 404 error when ValueError is thrown
+        #Raise 404 error from the string url
         raise Http404()
         assert False
     if date == dt.date.today():
-        return redirect(news_of_day)
-    news=Article.days_news(date)
-    
-    return render(request, 'all-news/past-news.html', {"date": date,'news':news})
-#..........
+        return redirect(news_of_day)    
+
+    news=Article.days_news(date)    
+
+    return render(request,'all-news/past-news.html',{'date':date,'news':news})
 
 
-def past_days_news(request,past_date):
-    try:
-        # Converts data from the string Url
-        date = dt.datetime.strptime(past_date, '%Y-%m-%d').date()
-    except ValueError:
-        # Raise 404 error when ValueError is thrown
-        raise Http404()
-        assert False
-
-    if date == dt.date.today():
-        return redirect(news_of_day)
-
-    news = Article.days_news(date)
-    return render(request, 'all-news/past-news.html',{"date": date,"news":news})
 def search_results(request):
-    #searches based on what is inputted 
-    
-    if 'article' in request.GET and request.GET["article"]:
-        search_term = request.GET.get("article")
-        searched_articles = Article.search_by_title(search_term)
-        message = f"{search_term}"
+    '''
+    view function that fetches articles based on search terms
+    '''  
 
-        return render(request, 'all-news/search.html',{"message":message,"articles": searched_articles})
+    if 'article' in request.GET and request.GET["article"]:
+        search_term=request.GET.get('article')
+        search_articles=Article.search_by_title(search_term)
+        message=f"{search_term}"
+
+        return render(request,'all-news/search.html',{'message':message,"articles":search_articles})
 
     else:
-        message = "You haven't searched for any term"
-        return render(request, 'all-news/search.html',{"message":message})
+        message='You havent searched for any term'
+        return render(request,'all-news/search.html',{'message':message})    
 
+@login_required(login_url='/accounts/login/')
 def article(request,article_id):
     try:
-        article= Article.objects.get(id=article_id)
+        article=Article.objects.get(id=article_id)
     except DoesNotExist:
         raise Http404()
-    return render (request, "all-news/article.html", {"article":article})
-    
+    return render (request,"all-news/article.html",{"article":article})   
+
+
+@login_required(login_url='/accounts/login/')
+def new_article(request):
+    current_user = request.user
+    if request.method == 'POST':
+        form = NewArticleForm(request.POST, request.FILES)
+        if form.is_valid():
+            article = form.save(commit=False)
+            article.editor = current_user
+            article.save()
+        return redirect('newsToday')
+
+    else:
+        form = NewArticleForm()
+    return render(request, 'new_article.html', {"form": form})
+
+
