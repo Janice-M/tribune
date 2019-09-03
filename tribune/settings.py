551cc627299bd7ffcb70a86a0fff5f42e0b654b1
Janice-M <janiceink001@gmail.com>
2019-09-03 18:33:42 +0300

    rest api installed

--- tribune/settings.py
+++ tribune/settings.py
@@ -36,6 +36,7 @@ INSTALLED_APPS = [
     'news',
     'bootstrap4',
     'tinymce',
+    'rest_framework',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
