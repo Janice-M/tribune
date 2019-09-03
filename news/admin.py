003e12e7a73b7673117b9d503766ab0abcf2106f
Janice-M <janiceink001@gmail.com>
2019-09-03 15:09:03 +0300

    editted admins.py removed editor

--- news/admin.py
+++ news/admin.py
@@ -1,11 +1,11 @@
 from django.contrib import admin
-from .models import Editor,Article,tags
+from .models import Article,Tags
 
 # Register your models here.
 
 class ArticleAdmin(admin.ModelAdmin):
     filter_horizontal =('tags',)
 
-admin.site.register(Editor)
-admin.site.register(Article)
-admin.site.register(tags)
+
+admin.site.register(Article,ArticleAdmin)
+admin.site.register(Tags)
